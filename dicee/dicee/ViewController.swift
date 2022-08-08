a6bcc1e3468fd8283c978997b73042de4f25b114
João Pedro Limão <johnazedo07@gmail.com>
2022-08-08 13:57:02 -0300

    [dicee] Change image with swift

--- dicee/dicee/ViewController.swift
+++ dicee/dicee/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceOneImage: UIImageView!
+    
+    @IBOutlet weak var diceTwoImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        diceOneImage.image = UIImage(named: "DiceSix")
     }
-
-
 }
 
