87d0c8f4772bda441153eee3f67e62db806daa2e
João Pedro Limão <johnazedo07@gmail.com>
2022-08-08 13:41:07 -0300

    :tada: [dicee] Initial commit

---
+++ dicee/dicee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dicee
+//
+//  Created by Joao Limao on 08/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
